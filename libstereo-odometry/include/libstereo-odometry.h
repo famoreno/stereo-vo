b5a2cd6248c6852183b4b7bd4d4164b8c59af60e
famoreno <famoreno@uma.es>
2015-12-09 13:38:22 +0100

    Remove unnecessary headers

--- libstereo-odometry/include/libstereo-odometry.h
+++ libstereo-odometry/include/libstereo-odometry.h
@@ -30,7 +30,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/features2d.hpp>
 
 // mrpt
 #include <mrpt/utils/CImage.h>
